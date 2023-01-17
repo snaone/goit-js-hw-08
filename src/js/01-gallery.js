37e4e2d0a3ea3a29d8d3fa8ee5d63004f53b2e9e
snaone <1stepsan@gmail.com>
2023-01-17 20:54:48 +0200

    sending for verification

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,39 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+const newGallery = createElement(galleryItems);
+
+
+galleryEl.insertAdjacentHTML('afterbegin', newGallery);
+
+function createElement(gallery) {
+    return gallery.map(({ preview, original, description }) => {
+        return `
+            <div class="gallery__item">
+            <a class="gallery__item" 
+            href="${original}">
+            <img class="gallery__image" 
+            src="${preview}" 
+            alt="${description}" />
+            </a>
+            </div>
+        `;
+    }).join('');
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+
+     captions: true,
+     captionSelector: 'img',
+     captionType: 'attr',
+     captionsData: 'alt',
+     captionDelay: 250,
+     preloading: true,
+     captionPosition: 'bottom',
+    });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import { throttle } from 'lodash.throttle';
 
+const formEl = document.querySelector('.feedback-form');
+const emailEl = document.querySelector('input[name="email"]');
+const messageEl = document.querySelector('textarea[name="message"]');
+const localKey = 'feedback-form-state';
+
+formEl.addEventListener('input', throttle(e => {
+    const feedbackEl = {emailEl: emailEl.value, messageEl: messageEl.value};
+    localStorage.setItem(localKey, JSON.stringify(feedbackEl));
+}, 500)
+);
+
+formEl.addEventListener('submit', e => {
+    e.preventDefault();
+    console.log({emailEl: emailEl.value, messageEl: messageEl.value});
+    formEl.reset();
+    localStorage.removeItem(localKey);
+});
+
+const loadPage = key => {
+    try {
+        const storageBank = localStorage.getItem(key);
+        return storageBank === null ? undefined : JSON.parse(storageBank);
+    } catch (error) {
+        console.error('Error :', error.message);
+    }
+};
+
+const localDataEl = loadPage(localKey);
+if (localDataEl) {
+    emailEl.value = localDataEl.emailEl;
+    messageEl.value = localDataEl.messageEl;
+}
